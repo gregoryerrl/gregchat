0f489a574927487c170985fdde89013dc2fd600d
Schvartzegreg <gregoryerrl@gmail.com>
2023-05-02 07:58:34 +0800

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,79 @@
+// Require necessary modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const socketio = require("socket.io");
+const bcrypt = require("bcryptjs");
 
+// Initialize app and body parser middleware
 const app = express();
-
-// Set up body-parser middleware
 app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
   .connect(
     "mongodb+srv://gregoryerrl05132000:%21%23QEgreg05132000@sample-chat-app.h1s9vsy.mongodb.net/test",
-    {useNewUrlParser: true}
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   )
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-// Set up Socket.io
-const server = app.listen(3000, () =>
-  console.log("Server running on port 3000")
-);
-const io = socketio(server);
-
-// Define the data model
-const messageSchema = new mongoose.Schema({
-  text: String,
-  user: String,
-  timestamp: {type: Date, default: Date.now},
-});
-const Message = mongoose.model("Message", messageSchema);
-
-// Create API endpoints
-app.get("/messages", (req, res) => {
-  Message.find({}, (err, messages) => {
-    if (err) return res.status(500).send(err);
-    res.send(messages);
-  });
+// Define User schema
+const userSchema = new mongoose.Schema({
+  username: {type: String, unique: true},
+  password: String,
 });
 
-app.post("/messages", (req, res) => {
-  const message = new Message(req.body);
-  message.save((err) => {
-    if (err) return res.status(500).send(err);
-    io.emit("message", req.body);
-    res.sendStatus(200);
-  });
+// Define User model
+const User = mongoose.model("User", userSchema);
+
+// POST request for registering new user
+app.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({username: req.body.username, password: hash});
+    await newUser.save();
+    res.status(201).json({message: "User created successfully"});
+  } catch (err) {
+    res.status(500).json({error: err.message});
+  }
 });
+
+// POST request for authenticating user
+app.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({username: req.body.username});
+    if (!user) throw new Error("User not found");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) throw new Error("Incorrect password");
+    res.status(200).json({message: "Login successful"});
+  } catch (err) {
+    res.status(401).json({error: err.message});
+  }
+});
+
+// Get all users
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json({
+      message: "All registered users fetched successfully",
+      users: users,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Server error",
+    });
+  }
+});
+
+// Start server
+app.listen(3000, () => console.log("Server started on port 3000"));
